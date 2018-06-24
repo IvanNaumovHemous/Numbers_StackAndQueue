068ef709307a171ab910d8825afd555091b13bef
Ivan <ivan.naumov92@yahoo.com>
2018-06-24 17:31:39 +0300

    Import

---
+++ NumbersWithStack.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NumbersWithaStack
+{
+    class NumbersWithStack
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine().Split(' ').Select(int.Parse);
+            var stack = new Stack<int>(text);
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop() + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
